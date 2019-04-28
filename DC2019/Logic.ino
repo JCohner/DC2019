e5c65dba8f825623546b2229a5e84226eaa2c626
JCohner <joshuacohen2019@u.northwestern.edu>
2019-04-28 16:55:43 -0500

    implemented per wheel reverse functionality also sect up structure for future dev

--- DC2019/Logic.ino
+++ DC2019/Logic.ino
@@ -1,10 +1,25 @@
+int effA;
+int effB;
 void process_command(char * c){
   Serial.println(read_char[0]);
+  
   if (read_char[0] == 'd'){
+    /*Reverse commands of form "d r a/b"*/
+    if (read_char[1] == 'r'){
+      if(read_char[2] == 'a'){
+        reverse_motor('a');
+        Serial.println("reversing motor A");
+      } else {
+        reverse_motor('b');
+        Serial.println("reversing motor B");
+      }
+    }
+    /*Motor effort commands of form "d motorAeff motorBeff"*/
     sscanf((read_char + 1), "%d %d", &effA, &effB);
     Serial.println(effA);
     Serial.println(effB);
     drive_motors(effA, effB);
+    
   } else if (read_char[0] = 'a'){
     digitalWrite(13, HIGH);
     Serial.println("suhhhhh");
